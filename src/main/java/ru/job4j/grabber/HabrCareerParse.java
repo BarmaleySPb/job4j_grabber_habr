5b11c223153b96df7cb08ab771a57a8767e40e88
Barmaley <e.g.zakharov@gmail.com>
2022-09-07 16:01:00 +0300

    add interfaces: Grab, Parse, Store

--- src/main/java/ru/job4j/grabber/HabrCareerParse.java
+++ src/main/java/ru/job4j/grabber/HabrCareerParse.java
@@ -5,6 +5,7 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import ru.job4j.grabber.utils.DateTimeParser;
 import ru.job4j.grabber.utils.HabrCareerDateTimeParser;
 
 import java.io.IOException;
@@ -12,9 +13,15 @@ import java.util.concurrent.atomic.AtomicReference;
 
 public class HabrCareerParse {
 
+    private final DateTimeParser dateTimeParser;
+
     private static final String SOURCE_LINK = "https://career.habr.com";
 
     private static final String PAGE_LINK = String.format("%s/vacancies/java_developer?page=", SOURCE_LINK);
+
+    public HabrCareerParse(DateTimeParser dateTimeParser) {
+        this.dateTimeParser = dateTimeParser;
+    }
 
     private String retrieveDescription(String link) throws IOException {
         AtomicReference<String> result = new AtomicReference<>("");
@@ -27,7 +34,8 @@ public class HabrCareerParse {
     }
 
     public static void main(String[] args) throws IOException {
-        HabrCareerDateTimeParser parser = new HabrCareerDateTimeParser();
+       // HabrCareerDateTimeParser parser = new HabrCareerDateTimeParser();
+
         for (int i = 1; i < 6; i++) {
             Connection connection = Jsoup.connect(PAGE_LINK + i);
             Document document = connection.get();
@@ -40,7 +48,7 @@ public class HabrCareerParse {
                 String vacancyName = titleElement.text();
                 String vacancyDate = String.format("%s", dateVacancy.attr("datetime"));
                 String link = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
-                System.out.printf("%s %s %s%n", vacancyName, link, parser.parse(vacancyDate));
+                System.out.printf("%s %s %s%n", vacancyName, link, vacancyDate);
             });
         }
     }
